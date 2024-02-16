721604365d0e0db56820ccebd9e5139d808da89e
Mohamed Walid <Moh.Walid2002@gmail.com>
2024-02-16 18:02:48 +0200

    Update aligned_dataset_vitonhd.py

--- data/aligned_dataset_vitonhd.py
+++ data/aligned_dataset_vitonhd.py
@@ -172,7 +172,9 @@ class AlignedDataset(BaseDataset):
         # person 2d pose
         print("\n\n\n\n\\n\n\n\tPATH\n\n\n\n\n\n")
         print(P_path)
-        pose_path = os.path.join('/kaggle/input/openposejson/openpose_json',os.path.basename(P_path)) # Modified line to meet kaggle requirements [OSError]
+        base_path = os.path.basename(P_path)
+        base_path_json = base_path.replace(".png",".json")
+        pose_path = os.path.join('/kaggle/input/openposejson/openpose_json',) # Modified line to meet kaggle requirements [OSError]
         #pose_path = P_path.replace('/image/', '/openpose_json/')[:-4]+'_keypoints.json'
         with open(pose_path, 'r') as f:
             datas = json.load(f)
